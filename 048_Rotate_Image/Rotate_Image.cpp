02ca7672464dca4bd8a933f22041fd95d9c102dc
csugulo <csugulo@gmail.com>
2019-03-03 04:38:20 +0800

    faster 048 solution

--- 048_Rotate_Image/Rotate_Image.cpp
+++ 048_Rotate_Image/Rotate_Image.cpp
@@ -1,11 +1,18 @@
 #include "leetcode_solutions.h"
 
+inline void swap(int & a, int & b) {
+	int tmp = a;
+	a = b;
+	b = tmp;
+}
+
 class Solution {
 public:
 	void rotate(vector<vector<int>>& matrix) {
 		size_t n = matrix.size();
 		for (size_t i = 0; i < n / 2; ++i)
-			swap(matrix[i], matrix[n - 1 - i]);
+			for(size_t j = 0; j < n; ++j)
+				swap(matrix[i][j], matrix[n - 1 - i][j]);
 		for (size_t row = 0; row < n - 1; ++row)
 			for (size_t col = row + 1; col < n; ++col)
 				swap(matrix[row][col], matrix[col][row]);
@@ -14,7 +21,7 @@ public:
 
 int main() {
 	Solution s;
-	vector<vector<int>> matrix = { {1} };
+	vector<vector<int>> matrix = { {1,2,3},{4,5,6},{7,8,9} };
 	s.rotate(matrix);
 	cout<<matrix;
 }
