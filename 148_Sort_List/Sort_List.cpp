c55ba81e77a1cb97ac1dd31cf035129ad1535bd7
csugulo <csugulo@users.noreply.github.com>
2019-03-28 00:03:20 +0800

    Update Sort_List.cpp

--- 148_Sort_List/Sort_List.cpp
+++ 148_Sort_List/Sort_List.cpp
@@ -2,47 +2,6 @@
 
 class Solution {
 public:
-<<<<<<< HEAD
-	ListNode* sortList(ListNode* head) {
-		if (head == NULL || head->next == NULL) return head;
-		return sort(head, NULL);
-
-	}
-
-	ListNode * sort(ListNode* begin, ListNode* end) {
-		ListNode *left(begin), *right(begin), *mid(begin);
-		if (begin->next == end) return begin;
-		ListNode *curr = begin->next;
-		begin->next = NULL;
-
-		while (curr != end) {
-			ListNode *next = curr->next;
-			curr->next = NULL;
-			if (curr->val < mid->val) {
-				curr->next = left;
-				left = curr;
-			}
-			else {
-				right->next = curr;
-				right = curr;
-			}
-			curr = next;
-		}
-		right->next = end;
-		ListNode *leftHead = sort(left, mid);
-		ListNode *rightHead = sort(mid->next, right->next);
-		return left;
-	}
-
-};
-
-int main() {
-	Solution s;
-	ListNode * head = CreateList({ 5,1,9,2 });
-	head = s.sortList(head);
-	cout << head;
-	return 0;
-=======
     ListNode* sortList(ListNode* head) {
         if(head == NULL || head -> next == NULL) return head;
         ListNode * dummy = new ListNode(0);
@@ -83,5 +42,4 @@ int main(){
     head =  s.sortList(head);
     cout << head;
     return 0;
->>>>>>> 8ef7bed87b36d65d17c54b68e707c57773567bca
 }
